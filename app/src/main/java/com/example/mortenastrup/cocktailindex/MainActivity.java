495eb8cefc7e9e0d6dadfa0866cc1f28efaf370c
Morten Astrup <morten.astrup@post.au.dk>
2019-02-27 19:37:30 +0100

    Starting

---
+++ app/src/main/java/com/example/mortenastrup/cocktailindex/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.mortenastrup.cocktailindex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.mortenastrup.cocktailindex.Fragments.FavoriteFragment;
+import com.example.mortenastrup.cocktailindex.Fragments.IdeaFragment;
+import com.example.mortenastrup.cocktailindex.Fragments.IndexFragment;
+
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener,
+        IndexFragment.OnFragmentInteractionListener,
+        FavoriteFragment.OnFragmentInteractionListener,
+        IdeaFragment.OnFragmentInteractionListener {
+
+    private BottomNavigationView bottomNavigationView;
+
+    // Fragments
+    private Fragment fragmentIndex;
+    private Fragment fragmentFavorite;
+    private Fragment fragmentIdea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SetupViews();   // FAB & BNV
+
+        // Instantiates the 3 base fragments (Index, Favourite & Idea)
+        fragmentIndex = new IndexFragment();
+        fragmentFavorite = new FavoriteFragment();
+        fragmentIdea = new IdeaFragment();
+
+        // Sets the current fragment as the IndexFragment
+        setCurrentFragment(fragmentIndex);
+    }
+
+    /**
+     * Switches the current fragment with the parameter fragment
+     * @param fragment  The fragment to be switched to
+     */
+    private void setCurrentFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Sets up the different views when creating the activity
+     */
+    private void SetupViews() {
+        // Setup of BottomNavigationView
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+
+
+        // Setup of FAB
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Intent intent;
+        switch (menuItem.getItemId()) {
+            case R.id.favorites:
+                setCurrentFragment(fragmentFavorite);
+                break;
+            case R.id.index:
+                setCurrentFragment(fragmentIndex);
+                break;
+            case R.id.ideas:
+                setCurrentFragment(fragmentIdea);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void indexFragmentInteractionListener(String task) {
+
+    }
+
+    @Override
+    public void favoriteFragmentInteractionListener(String task) {
+
+    }
+
+    @Override
+    public void ideaFragmentInteractionListener(String task) {
+
+    }
+}

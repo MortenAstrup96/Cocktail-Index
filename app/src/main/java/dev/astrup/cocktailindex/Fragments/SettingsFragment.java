b3dce7b56061fd9ce4f3055a1bbab1a527184852
Morten Astrup <morten.astrup@post.au.dk>
2019-07-21 21:19:57 +0200

    Dialog now working after resetting privacy settings

--- app/src/main/java/dev/astrup/cocktailindex/Fragments/SettingsFragment.java
+++ app/src/main/java/dev/astrup/cocktailindex/Fragments/SettingsFragment.java
@@ -2,15 +2,32 @@ package dev.astrup.cocktailindex.Fragments;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import dev.astrup.cocktailindex.R;
+import dev.astrup.cocktailindex.Utility.GdprHelper;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference button = findPreference(getString(R.string.resetPrivacy));
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getContext(), "Privacy Settings Reset", Toast.LENGTH_LONG).show();
+                GdprHelper gdprHelper = new GdprHelper(getContext());
+                gdprHelper.resetConsent();
+
+                gdprHelper.initialise();
+                return true;
+            }
+        });
     }
 
 
